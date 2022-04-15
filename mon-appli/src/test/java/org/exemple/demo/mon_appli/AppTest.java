0ad39c003e05f816cd85558aa70edf87dfd59c47
Théo Colin <theo.colin@alumni.univ-avignon.fr>
2022-04-15 14:31:56 +0200

    Ajout du projet maven

---
+++ mon-appli/src/test/java/org/exemple/demo/mon_appli/AppTest.java
@@ -0,0 +1,20 @@
+package org.exemple.demo.mon_appli;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
