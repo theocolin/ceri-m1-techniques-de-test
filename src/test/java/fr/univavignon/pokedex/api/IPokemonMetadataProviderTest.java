8f47dbbe3b763f597c4049d9dd315957855cb997
Théo Colin <theo.colin@alumni.univ-avignon.fr>
2022-05-04 17:18:44 +0200

    Ajout du test IPokemonMetadataProviderTest.

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,10 +1,42 @@
 package fr.univavignon.pokedex.api;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
-public class IPokemonMetadataProviderTest extends TestCase {
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
+public class IPokemonMetadataProviderTest {
 
-    public void testGetPokemonMetadata() {
+    @Mock
+    IPokemonMetadataProvider pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+    PokemonMetadata bulbi = new PokemonMetadata(0,"Bulbizarre", 126, 126, 90);
+    PokemonMetadata aqua = new PokemonMetadata(133,"Aquali", 186, 168, 260);
+
+    @Before
+    public void setUp() {
+        try {
+            when(pokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(bulbi);
+            when(pokemonMetadataProvider.getPokemonMetadata(133)).thenReturn(aqua);
+            
+            Mockito.doThrow(new PokedexException("Index out of bound")).when(pokemonMetadataProvider).getPokemonMetadata(Mockito.intThat(i -> i < 0 || i > 150));
+        } catch (PokedexException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetPokemonMetadata() throws PokedexException {
+        try {
+            assertEquals(bulbi, pokemonMetadataProvider.getPokemonMetadata(0));
+            assertThrows(PokedexException.class, (ThrowingRunnable) pokemonMetadataProvider.getPokemonMetadata(-1));
+            assertThrows(PokedexException.class, (ThrowingRunnable) pokemonMetadataProvider.getPokemonMetadata(200));
+        }catch (PokedexException e){
+            e.printStackTrace();
+        }
     }
 }
