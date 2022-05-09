76bf706a7b017239e060840262feba24229a440e
Théo Colin <theo.colin@alumni.univ-avignon.fr>
2022-05-09 09:45:00 +0200

    Ajout des tests sur IPokedexFactoryTest.

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -6,17 +6,23 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import static org.mockito.Mockito.when;
+
 public class IPokedexFactoryTest extends TestCase {
 
     @Mock
     IPokedexFactory pokedex = Mockito.mock(IPokedexFactory.class);
+    @Mock
+    IPokemonFactory iPokemonFactoryMock = Mockito.mock(IPokemonFactory.class);
+    @Mock
+    IPokemonMetadataProvider iPokemonMetadataProviderMock = Mockito.mock(IPokemonMetadataProvider.class);
+    @Mock
+    IPokedex iPokedexMock = Mockito.mock(IPokedex.class);
 
-    @Before
-    public void init() {
-
-    }
 
     @Test
     public void testCreatePokedex() {
+        when(pokedex.createPokedex(iPokemonMetadataProviderMock, iPokemonFactoryMock)).thenReturn(iPokedexMock);
+        assertEquals(iPokedexMock, pokedex.createPokedex(iPokemonMetadataProviderMock, iPokemonFactoryMock));
     }
 }
