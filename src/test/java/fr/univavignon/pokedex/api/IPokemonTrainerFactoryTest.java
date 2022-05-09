bb10fbb8f2eaae72985fd0101cede3b3f7d3b0de
Théo Colin <theo.colin@alumni.univ-avignon.fr>
2022-05-09 09:52:30 +0200

    Ajout des tests sur IPokemonTrainerFactoryTest.

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,7 +1,30 @@
 package fr.univavignon.pokedex.api;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class IPokemonTrainerFactoryTest {
+    @Mock
+    IPokemonTrainerFactory pokemonTrainerFactoryMock = Mockito.mock(IPokemonTrainerFactory.class);
+    @Mock
+    IPokedexFactory pokedexFactoryMock = Mockito.mock(IPokedexFactory.class);
+    @Mock
+    IPokedex pokedexMock = Mockito.mock(IPokedex.class);
+    PokemonTrainer pokemonTrainer = new PokemonTrainer("Theo", Team.INSTINCT, pokedexMock);
 
+    @Before
+    public void setUp() {
+        when(pokemonTrainerFactoryMock.createTrainer("Theo", Team.INSTINCT, pokedexFactoryMock)).thenReturn(pokemonTrainer);
+    }
+
+    @Test
+    public void testCreateTrainer() {
+        assertEquals(pokemonTrainer, pokemonTrainerFactoryMock.createTrainer("Theo", Team.INSTINCT, pokedexFactoryMock));
+    }
 }
