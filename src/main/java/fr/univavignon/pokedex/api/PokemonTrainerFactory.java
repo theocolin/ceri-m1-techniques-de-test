ee64850236f93c66286aa9f34d6690d6f19e0fcf
Théo Colin <theo.colin@alumni.univ-avignon.fr>
2022-05-13 09:37:52 +0200

    Implement PokemonTrainerFactory

---
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -0,0 +1,9 @@
+package fr.univavignon.pokedex.api;
+
+public class PokemonTrainerFactory implements IPokemonTrainerFactory {
+    @Override
+    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+        IPokedex pokedex = pokedexFactory.createPokedex(PokemonMetadataProvider.getPokemonMetadataProvider(), new PokemonFactory());
+        return new PokemonTrainer(name, team, pokedex);
+    }
+}
